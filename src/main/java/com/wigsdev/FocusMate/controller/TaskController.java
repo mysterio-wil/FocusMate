8c349bce7f4086b8a2655cfea6e4ae77d2ee16dd
Wilmer Gulcochia <karlwgs1989@gmail.com>
2025-04-26 20:38:18 -0500

    otro commit

--- src/main/java/com/wigsdev/FocusMate/controller/TaskController.java
+++ src/main/java/com/wigsdev/FocusMate/controller/TaskController.java
@@ -1,5 +1,20 @@
 package com.wigsdev.FocusMate.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
+
 public class TaskController {
-    // Lógica del gestor de tareas irá aquí
+    @FXML
+    private HBox pomodoroMiniContainer;
+
+    public void initialize() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/pomodoro_mini.fxml"));
+            HBox miniWidget = loader.load();
+            pomodoroMiniContainer.getChildren().add(miniWidget);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
