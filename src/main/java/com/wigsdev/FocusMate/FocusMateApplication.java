5a902fc709ebb7475c969a10b43952a388602cfd
Wilmer Gulcochia <karlwgs1989@gmail.com>
2025-04-27 19:41:15 -0500

    otro commit MateApplication.java

--- src/main/java/com/wigsdev/FocusMate/FocusMateApplication.java
+++ src/main/java/com/wigsdev/FocusMate/FocusMateApplication.java
@@ -1,13 +1,21 @@
 package com.wigsdev.FocusMate;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-@SpringBootApplication
-public class FocusMateApplication {
+public class FocusMateApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/tasks.fxml"));
+        primaryStage.setTitle("FocusMate");
+        primaryStage.setScene(new Scene(root, 1200, 700));
+        primaryStage.show();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(FocusMateApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
